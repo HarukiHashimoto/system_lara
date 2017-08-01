8a9779e26f5419209d1d32da9c87e6e31e6bcc29
HarukiHashimoto <coral.reef610@gmail.com>
2017-08-01 16:20:31 +0900

    update

--- public/js/sytemsend.js
+++ public/js/sytemsend.js
@@ -1,21 +1,19 @@
 $(function() {
-    $('#fetch').click(function() {
-        $.ajax({
-            type: "POST",
-            url: "systemsend",
-            data: {'action': "action"},
-        }).done(function(lm_list) {
-            var lm_list = JSON.parse(lm_list);
-        //    alert(lm_list.length);
-           var target = document.getElementById('lm_title');
-           for (var i = 0; i < lm_list.length; i++) {
-               target.innerHTML += "<div class='ui card centered'><div class='content'><div class='header'>"+lm_list[i].title+"</div></div><div class='extra content'><div class='ui basic green button'>読む</div></div></div><br />";
-           }
-       }).fail(function(jqXHR, textStatus, errorThrown) {
-          $("#XMLHttpRequest").html("XMLHttpRequest : " + jqXHR.status);
-          $("#textStatus").html("textStatus : " + textStatus);
-          $("#errorThrown").html("errorThrown : " + errorThrown);
-      });
+    $.ajax({
+        type: "GET",
+        url: "systemsend",
+        data: {'action': "action"},
+    }).done(function(lm_list) {
+        var lm_list = JSON.parse(lm_list);
+    //    alert(lm_list.length);
+       var target = document.getElementById('lm_title');
+       for (var i = 0; i < lm_list.length; i++) {
+           target.innerHTML += "<div class='ui card centered'><div class='content'><div class='header'>"+lm_list[i].title+"</div></div><div class='extra content'><div class='ui basic green button'>読む</div></div></div><br />";
+       }
+   }).fail(function(jqXHR, textStatus, errorThrown) {
+      $("#XMLHttpRequest").html("XMLHttpRequest : " + jqXHR.status);
+      $("#textStatus").html("textStatus : " + textStatus);
+      $("#errorThrown").html("errorThrown : " + errorThrown);
     });
     return false;
 });
