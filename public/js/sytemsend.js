19e6999129135df54dd8d3819e1787b1190cc746
橋本陽生 <coral.reef610@gmail.com>
2017-08-03 18:35:00 +0900

    update

--- public/js/sytemsend.js
+++ public/js/sytemsend.js
@@ -1,19 +1,33 @@
 $(function() {
+    // データベースから教材リストを取得
     $.ajax({
         type: "GET",
-        url: "systemsend",
-        data: {'action': "action"},
+        url: "read",
     }).done(function(lm_list) {
         var lm_list = JSON.parse(lm_list);
         var target = document.getElementById('lm_title');
         for (var i = 0; i < lm_list.length; i++) {
-            target.innerHTML += "<div class='ui card centered'><div class='content'><div class='header'>"+lm_list[i].title+"</div></div><div class='extra content'><div class='ui basic green button' value="+lm_list[i].id+">読む</div></div></div><br />";
+            target.innerHTML += "<div class='ui card centered'><div class='content'><div class='header'>"+lm_list[i].title+"</div></div><div class='extra content'><button type='submit' class='ui button basic lm_btn' value="+lm_list[i].material_id+">READ</button></div></div><br />";
        }
+
+       $('.lm_btn').hover(function() {
+           var lm_id = event.srcElement.value;
+           console.log(lm_id);
+           var form = document.forms.mainForm;
+           form.setAttribute("action","main/"+lm_id);
+       });
+
+    //    $('.lm_btn').on('click', function() {
+    //        var lm_id = event.srcElement.value;
+    //        console.log(lm_id);
+    //    });
+
    }).fail(function(jqXHR, textStatus, errorThrown) {
        $("#XMLHttpRequest").html("XMLHttpRequest : " + jqXHR.status);
        $("#textStatus").html("textStatus : " + textStatus);
        $("#errorThrown").html("errorThrown : " + errorThrown);
     });
+
     return false;
 
 
--- resources/views/system.blade.php
+++ resources/views/system.blade.php
@@ -1,19 +1,19 @@
 @extends('layout.master')
 @section('content')
-    <div>
-        <br />
-        <div id="lm_title">
-
-        </div>
-        <p class='centered'>
-        <button type='submit' name='action' value='action' class='ui primary button' id='fetch'>教材選択</button>
-        </p>
-        {{-- @for ($i=0; $i < count($lm_title); $i++)
-            <p class="centered" id="lm_title" name={{$lm_id[$i]}}>
-                {{$lm_title[$i]}}
+    <form method="get" action="" name="mainForm">
+        <div>
+            <br />
+            <div id="lm_title">
+            </div>
+            <p class='centered'>
             </p>
-        @endfor --}}
-        <br />
-        <br />
-    </div>
+            {{-- @for ($i=0; $i < count($lm_title); $i++)
+                <p class="centered" id="lm_title" name={{$lm_id[$i]}}>
+                    {{$lm_title[$i]}}
+                </p>
+            @endfor --}}
+            <br />
+            <br />
+        </div>
+    </form>
 @endsection
