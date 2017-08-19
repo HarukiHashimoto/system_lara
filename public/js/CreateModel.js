7bf86319407e2f1a8863611ddbe8ee066562e054
HarukiHaruki <coral.reef610@gmail.com>
2017-08-20 02:19:12 +0900

    update

--- public/js/CreateModel.js
+++ public/js/CreateModel.js
@@ -127,8 +127,10 @@ $.ajax({
           if (label[i].checked) {
               if(label[i].value == 1) {
                   data.color = {color:'blue'};
-              } else {
+              } else if(label[i].value == 2) {
                   data.color = {color:'red'};
+              } else {
+                  data.color = {color:'#747474'};
               }
           }
       }
@@ -158,8 +160,10 @@ $.ajax({
           if (label[i].checked) {
               if(label[i].value == 1) {
                   data.color = {color:'blue'};
-              } else {
+              } else if(label[i].value == 2) {
                   data.color = {color:'red'};
+              } else {
+                  data.color = {color:'#747474'};
               }
 
           }
